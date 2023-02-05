69ee91469e29f3c5251702f9a498160bd75ba7ee
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2023-02-05 21:11:42 +0530

    creating initial login and calender page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
+import Head from 'next/head'
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+    <Head>
+        <title>Apple Replica</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-  return <Component {...pageProps} />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,9 @@
-import Head from 'next/head'
-
 const Home = () => {
   return (
-    <div>
-      <Head>
-        <title>Apple Replica</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="h-[100vh] w-full flex flex-col justify-center items-center text-center px-10">
+      <h1 className="text-gray-800 text-5xl font-bold">Please Sign In to use this webapp</h1>
 
-      <h1>lets build apple replica</h1>
+      <p className="py-3">authenticate yourself using your <span className="underline">google</span> account</p>
     </div>
   )
 }
