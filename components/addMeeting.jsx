16eef2b94bb3376375b8a59477631a75ee1beed5
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2023-02-20 23:00:40 +0530

    refactoring code

---
+++ components/addMeeting.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddMeeting = () => {
+    return (
+        <div>
+            addmeetings
+        </div>
+    )
+}
+
+export default AddMeeting
---
+++ components/meetings.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Meeting = () => {
+    return (
+        <div>
+            meetings.map
+        </div>
+    )
+}
+
+export default Meeting
